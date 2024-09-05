4f8704269450d8bcb7d51d0abd12230fb25d06a0
Anthony <anthonyadasme.c@gmail.com>
2024-09-05 13:37:46 -0400

    agregando bootstrap

---
+++ js.js
@@ -0,0 +1,16 @@
+function MyComponent() {
+    return (
+      <Container>
+        <Container className="container__header shadow rounded">
+          {/* Header content */}
+        </Container>
+        <Row>
+          <Col>
+            <section className="section__main">
+              <div className="shadow rounded"> </div>
+            </section>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
